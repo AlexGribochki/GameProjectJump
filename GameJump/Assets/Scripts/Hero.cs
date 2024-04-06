99f5d41e8d579b3d32785245daf972189bfd7032
AlexGribochki <117588340+AlexGribochki@users.noreply.github.com>
2024-04-06 10:57:09 +0500

    Update Hero scripts

--- GameJump/Assets/Scripts/Hero.cs
+++ GameJump/Assets/Scripts/Hero.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 3f; // скорость героя
+    //[SerializeField] private int lives = 3; // количество жизни
+    [SerializeField] private float jumpForce = 15f; // сила прыжка
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private States State
     {
-        
+        get { return (States)anim.GetInteger("State"); }
+        set { anim.SetInteger("State", (int)value); }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isGrounded) State = States.idle;
+
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (isGrounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Run()
+    {
+        if (isGrounded) State = States.run;
+
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+
+        if (!isGrounded) State = States.jump;
+    }
+}
+
+public enum States
+{
+    idle,
+    run,
+    jump
+}
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 pos;
+
+    private void Awake()
+    {
+        if (!player)
+            player = FindObjectOfType<Hero>().transform;
+    }
+
+    private void Update()
+    {
+        pos = player.position;
+        pos.z = -10f;
+
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
     }
 }
